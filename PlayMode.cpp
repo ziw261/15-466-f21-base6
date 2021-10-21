db758a3e4425689276b4e8b3fb43b9a8a5c554f9
Azusa <akiriin0@gmail.com>
2021-10-21 01:18:34 -0400

    Some basic opengl setup

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -1,5 +1,5 @@
 #include "PlayMode.hpp"
-
+#include <signal.h>
 #include "DrawLines.hpp"
 #include "gl_errors.hpp"
 #include "data_path.hpp"
@@ -9,7 +9,125 @@
 
 #include <random>
 
+#ifdef _MSC_VER
+#define DEBUG_BREAK __debugbreak()
+#elif __APPLE__
+#define DEBUG_BREAK __builtin_trap()
+#else
+#define DEBUG_BREAK raise(SIGTRAP)
+#endif
+
+#define ASSERT(x) if (!(x)) DEBUG_BREAK;
+#define GLCall(x) GLClearError();\
+	x;\
+	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+
+static void GLClearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+	while (GLenum error = glGetError())
+	{
+		std::cout << "[OpenGL Error] ( " << error << " ) : " << function << " " << file << ": " << line << std::endl;
+		return false;
+	}
+	return true;
+}
+
+
+
 PlayMode::PlayMode(Client &client_) : client(client_) {
+
+	//----- allocate OpenGL resources -----
+	{ //vertex buffer:
+		glGenBuffers(1, &vertex_buffer);
+		//for now, buffer will be un-filled.
+
+		GL_ERRORS(); //PARANOIA: print out any OpenGL errors that may have happened
+	}
+
+	{ //vertex array mapping buffer for color_texture_program:
+		//ask OpenGL to fill vertex_buffer_for_color_texture_program with the name of an unused vertex array object:
+		glGenVertexArrays(1, &vertex_buffer_for_color_texture_program);
+
+		//set vertex_buffer_for_color_texture_program as the current vertex array object:
+		glBindVertexArray(vertex_buffer_for_color_texture_program);
+
+		//set vertex_buffer as the source of glVertexAttribPointer() commands:
+		glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+
+		//set up the vertex array object to describe arrays of PongMode::Vertex:
+		glVertexAttribPointer(
+			color_texture_program.Position_vec4, //attribute
+			3, //size
+			GL_FLOAT, //type
+			GL_FALSE, //normalized
+			sizeof(Vertex), //stride
+			(GLbyte*)0 + 0 //offset
+		);
+		glEnableVertexAttribArray(color_texture_program.Position_vec4);
+		//[Note that it is okay to bind a vec3 input to a vec4 attribute -- the w component will be filled with 1.0 automatically]
+
+		glVertexAttribPointer(
+			color_texture_program.Color_vec4, //attribute
+			4, //size
+			GL_UNSIGNED_BYTE, //type
+			GL_TRUE, //normalized
+			sizeof(Vertex), //stride
+			(GLbyte*)0 + 4 * 3 //offset
+		);
+		glEnableVertexAttribArray(color_texture_program.Color_vec4);
+
+		glVertexAttribPointer(
+			color_texture_program.TexCoord_vec2, //attribute
+			2, //size
+			GL_FLOAT, //type
+			GL_FALSE, //normalized
+			sizeof(Vertex), //stride
+			(GLbyte*)0 + 4 * 3 + 4 * 1 //offset
+		);
+		glEnableVertexAttribArray(color_texture_program.TexCoord_vec2);
+
+		//done referring to vertex_buffer, so unbind it:
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		//done setting up vertex array object, so unbind it:
+		glBindVertexArray(0);
+
+		GL_ERRORS(); //PARANOIA: print out any OpenGL errors that may have happened
+	}
+
+	{ //solid white texture:
+		//ask OpenGL to fill white_tex with the name of an unused texture object:
+		glGenTextures(1, &white_tex);
+
+		//bind that texture object as a GL_TEXTURE_2D-type texture:
+		glBindTexture(GL_TEXTURE_2D, white_tex);
+
+		//upload a 1x1 image of solid white to the texture:
+		glm::uvec2 size = glm::uvec2(1, 1);
+		std::vector< glm::u8vec4 > data(size.x * size.y, glm::u8vec4(0xff, 0xff, 0xff, 0xff));
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data.data());
+
+		//set filtering and wrapping parameters:
+		//(it's a bit silly to mipmap a 1x1 texture, but I'm doing it because you may want to use this code to load different sizes of texture)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		//since texture uses a mipmap and we haven't uploaded one, instruct opengl to make one for us:
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		//Okay, texture uploaded, can unbind it:
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+		GL_ERRORS(); //PARANOIA: print out any OpenGL errors that may have happened
+	}
 }
 
 PlayMode::~PlayMode() {
@@ -105,35 +223,170 @@ void PlayMode::update(float elapsed) {
 	}, 0.0);
 }
 
+void PlayMode::draw_chessboard() {
+
+
+
+}
+
 void PlayMode::draw(glm::uvec2 const &drawable_size) {
 	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	{ //use DrawLines to overlay some text:
-		glDisable(GL_DEPTH_TEST);
-		float aspect = float(drawable_size.x) / float(drawable_size.y);
-		DrawLines lines(glm::mat4(
-			1.0f / aspect, 0.0f, 0.0f, 0.0f,
-			0.0f, 1.0f, 0.0f, 0.0f,
-			0.0f, 0.0f, 1.0f, 0.0f,
-			0.0f, 0.0f, 0.0f, 1.0f
-		));
+	//{ //use DrawLines to overlay some text:
+	//	glDisable(GL_DEPTH_TEST);
+	//	float aspect = float(drawable_size.x) / float(drawable_size.y);
+	//	DrawLines lines(glm::mat4(
+	//		1.0f / aspect, 0.0f, 0.0f, 0.0f,
+	//		0.0f, 1.0f, 0.0f, 0.0f,
+	//		0.0f, 0.0f, 1.0f, 0.0f,
+	//		0.0f, 0.0f, 0.0f, 1.0f
+	//	));
 
-		auto draw_text = [&](glm::vec2 const &at, std::string const &text, float H) {
-			lines.draw_text(text,
-				glm::vec3(at.x, at.y, 0.0),
-				glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
-				glm::u8vec4(0x00, 0x00, 0x00, 0x00));
-			float ofs = 2.0f / drawable_size.y;
-			lines.draw_text(text,
-				glm::vec3(at.x + ofs, at.y + ofs, 0.0),
-				glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
-				glm::u8vec4(0xff, 0xff, 0xff, 0x00));
+	//	auto draw_text = [&](glm::vec2 const &at, std::string const &text, float H) {
+	//		lines.draw_text(text,
+	//			glm::vec3(at.x, at.y, 0.0),
+	//			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
+	//			glm::u8vec4(0x00, 0x00, 0x00, 0x00));
+	//		float ofs = 2.0f / drawable_size.y;
+	//		lines.draw_text(text,
+	//			glm::vec3(at.x + ofs, at.y + ofs, 0.0),
+	//			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
+	//			glm::u8vec4(0xff, 0xff, 0xff, 0x00));
+	//	};
+
+	//	draw_text(glm::vec2(-aspect + 0.1f, 0.0f), server_message, 0.09f);
+
+	//	draw_text(glm::vec2(-aspect + 0.1f,-0.9f), "(press WASD to change your total)", 0.09f);
+	//}
+
+
+	{
+
+		std::vector< Vertex > vertices;
+
+		#define HEX_TO_U8VEC4( HX ) (glm::u8vec4( (HX >> 24) & 0xff, (HX >> 16) & 0xff, (HX >> 8) & 0xff, (HX) & 0xff ))
+		const glm::u8vec4 bg_color = HEX_TO_U8VEC4(0x193b59ff);
+		const glm::u8vec4 fg_color = HEX_TO_U8VEC4(0xf44336ff);
+		const glm::u8vec4 shadow_color = HEX_TO_U8VEC4(0xf2ad94ff);
+		const std::vector< glm::u8vec4 > trail_colors = {
+			HEX_TO_U8VEC4(0xf2ad9488),
+			HEX_TO_U8VEC4(0xf2897288),
+			HEX_TO_U8VEC4(0xbacac088),
+		};
+		#undef HEX_TO_U8VEC4
+
+
+		auto draw_rectangle = [&vertices](glm::vec2 const& center, glm::vec2 const& radius, glm::u8vec4 const& color) {
+			//draw rectangle as two CCW-oriented triangles:
+			vertices.emplace_back(glm::vec3(center.x - radius.x, center.y - radius.y, 0.0f), color, glm::vec2(0.5f, 0.5f));
+			vertices.emplace_back(glm::vec3(center.x + radius.x, center.y - radius.y, 0.0f), color, glm::vec2(0.5f, 0.5f));
+			vertices.emplace_back(glm::vec3(center.x + radius.x, center.y + radius.y, 0.0f), color, glm::vec2(0.5f, 0.5f));
+
+			vertices.emplace_back(glm::vec3(center.x - radius.x, center.y - radius.y, 0.0f), color, glm::vec2(0.5f, 0.5f));
+			vertices.emplace_back(glm::vec3(center.x + radius.x, center.y + radius.y, 0.0f), color, glm::vec2(0.5f, 0.5f));
+			vertices.emplace_back(glm::vec3(center.x - radius.x, center.y + radius.y, 0.0f), color, glm::vec2(0.5f, 0.5f));
 		};
 
-		draw_text(glm::vec2(-aspect + 0.1f, 0.0f), server_message, 0.09f);
+		//draw_rectangle(glm::vec3(0, 0, 0), glm::vec2(0.75f, 0.75f), bg_color);
 
-		draw_text(glm::vec2(-aspect + 0.1f,-0.9f), "(press WASD to change your total)", 0.09f);
+
+		glm::vec2 court_radius = glm::vec2(7.0f, 5.0f);
+		const float wall_radius = 0.05f;
+		const float padding = 0.14f; //padding between outside of walls and edge of window
+
+		//walls:
+		draw_rectangle(glm::vec2(-court_radius.x - wall_radius, 0.0f), glm::vec2(wall_radius, court_radius.y + 2.0f * wall_radius), fg_color);
+		draw_rectangle(glm::vec2(court_radius.x + wall_radius, 0.0f), glm::vec2(wall_radius, court_radius.y + 2.0f * wall_radius), fg_color);
+		draw_rectangle(glm::vec2(0.0f, -court_radius.y - wall_radius), glm::vec2(court_radius.x, wall_radius), fg_color);
+		draw_rectangle(glm::vec2(0.0f, court_radius.y + wall_radius), glm::vec2(court_radius.x, wall_radius), fg_color);
+
+
+
+
+		//------ compute court-to-window transform ------
+
+		//compute area that should be visible:
+		glm::vec2 scene_min = glm::vec2(
+			-court_radius.x - 2.0f * wall_radius - padding,
+			-court_radius.y - 2.0f * wall_radius - padding
+		);
+		glm::vec2 scene_max = glm::vec2(
+			court_radius.x + 2.0f * wall_radius + padding,
+			court_radius.y + 2.0f * wall_radius + padding
+		);
+
+		//compute window aspect ratio:
+		float aspect = drawable_size.x / float(drawable_size.y);
+		//we'll scale the x coordinate by 1.0 / aspect to make sure things stay square.
+
+		//compute scale factor for court given that...
+		float scale = std::min(
+			(2.0f * aspect) / (scene_max.x - scene_min.x), //... x must fit in [-aspect,aspect] ...
+			(2.0f) / (scene_max.y - scene_min.y) //... y must fit in [-1,1].
+		);
+
+		glm::vec2 center = 0.5f * (scene_max + scene_min);
+
+		//build matrix that scales and translates appropriately:
+		glm::mat4 court_to_clip = glm::mat4(
+			glm::vec4(scale / aspect, 0.0f, 0.0f, 0.0f),
+			glm::vec4(0.0f, scale, 0.0f, 0.0f),
+			glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
+			glm::vec4(-center.x * (scale / aspect), -center.y * scale, 0.0f, 1.0f)
+		);
+		//NOTE: glm matrices are specified in *Column-Major* order,
+		// so each line above is specifying a *column* of the matrix(!)
+
+		//also build the matrix that takes clip coordinates to court coordinates (used for mouse handling):
+		clip_to_court = glm::mat3x2(
+			glm::vec2(aspect / scale, 0.0f),
+			glm::vec2(0.0f, 1.0f / scale),
+			glm::vec2(center.x, center.y)
+		);
+
+
+		//---- actual drawing ----
+
+		//clear the color buffer:
+		GLCall(glClearColor(bg_color.r / 255.0f, bg_color.g / 255.0f, bg_color.b / 255.0f, bg_color.a / 255.0f));
+		GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+		//use alpha blending:
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+		//don't use the depth test:
+		GLCall(glDisable(GL_DEPTH_TEST));
+
+		//upload vertices to vertex_buffer:
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer)); //set vertex_buffer as current
+		GLCall(glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), vertices.data(), GL_STREAM_DRAW)); //upload vertices array
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+
+		//set color_texture_program as current program:
+		GLCall(glUseProgram(color_texture_program.program));
+
+		//upload OBJECT_TO_CLIP to the proper uniform location:
+		GLCall(glUniformMatrix4fv(color_texture_program.OBJECT_TO_CLIP_mat4, 1, GL_FALSE, glm::value_ptr(court_to_clip)));
+		
+		//use the mapping vertex_buffer_for_color_texture_program to fetch vertex data:
+		GLCall(glBindVertexArray(vertex_buffer_for_color_texture_program));
+
+		//bind the solid white texture to location zero so things will be drawn just with their colors:
+		GLCall(glActiveTexture(GL_TEXTURE0));
+		GLCall(glBindTexture(GL_TEXTURE_2D, white_tex));
+
+		//run the OpenGL pipeline:
+		GLCall(glDrawArrays(GL_TRIANGLES, 0, GLsizei(vertices.size())));
+
+		//unbind the solid white texture:
+		GLCall(glBindTexture(GL_TEXTURE_2D, 0));
+
+		//reset vertex array to none:
+		GLCall(glBindVertexArray(0));
+
+		//reset current program to none:
+		GLCall(glUseProgram(0));
 	}
 	GL_ERRORS();
 }
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -2,6 +2,7 @@
 
 #include "Connection.hpp"
 
+#include "ColorTextureProgram.hpp"
 #include <glm/glm.hpp>
 
 #include <vector>
@@ -27,7 +28,35 @@ struct PlayMode : Mode {
 	//last message from server:
 	std::string server_message;
 
+	void draw_chessboard();
+
 	//connection to server:
 	Client &client;
+
+	struct Vertex {
+		Vertex(glm::vec3 const& Position_, glm::u8vec4 const& Color_, glm::vec2 const& TexCoord_) :
+			Position(Position_), Color(Color_), TexCoord(TexCoord_) { }
+		glm::vec3 Position;
+		glm::u8vec4 Color;
+		glm::vec2 TexCoord;
+	};
+	static_assert(sizeof(Vertex) == 4 * 3 + 1 * 4 + 4 * 2, "Vertex should be packed");
+
+	//Shader program that draws transformed, vertices tinted with vertex colors:
+	ColorTextureProgram color_texture_program;
+
+	//Buffer used to hold vertex data during drawing:
+	GLuint vertex_buffer = 0;
+
+	//Vertex Array Object that maps buffer locations to color_texture_program attribute locations:
+	GLuint vertex_buffer_for_color_texture_program = 0;
+
+	//Solid white texture:
+	GLuint white_tex = 0;
+
+	//matrix that maps from clip coordinates to court-space coordinates:
+	glm::mat3x2 clip_to_court = glm::mat3x2(1.0f);
+	// computed in draw() as the inverse of OBJECT_TO_CLIP
+	// (stored here so that the mouse handling code can use it to position the paddle)
 
 };
